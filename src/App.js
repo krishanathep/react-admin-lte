2720ac1d21a401e53a5956aa831427bc43d021ee
krishanathep <krishanathep@gmail.com>
2020-07-22 13:58:36 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Content from './components/Content'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Sidebar/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
