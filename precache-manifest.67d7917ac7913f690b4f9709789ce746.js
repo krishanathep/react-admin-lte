271c63a1ae2e913ad8fa63f1b2090e1a750fcb54
krishanathep <krishanathep@gmail.com>
2020-07-24 16:39:24 +0700

    Updates

---
+++ precache-manifest.67d7917ac7913f690b4f9709789ce746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b2e1896dcd875438c3cb5fffe951b7",
+    "url": "/react-admin-lte/index.html"
+  },
+  {
+    "revision": "705d83e1829b0f1d6fc4",
+    "url": "/react-admin-lte/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cde3fd89efa11e8111b0",
+    "url": "/react-admin-lte/static/js/2.fd4a4690.chunk.js"
+  },
+  {
+    "revision": "5be1595e26cd5eb9716fa620c026ffc5",
+    "url": "/react-admin-lte/static/js/2.fd4a4690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "705d83e1829b0f1d6fc4",
+    "url": "/react-admin-lte/static/js/main.8b9625b9.chunk.js"
+  },
+  {
+    "revision": "90b8b8f4aa90877fc45a",
+    "url": "/react-admin-lte/static/js/runtime-main.87933f64.js"
+  }
+]);
