98960fdeb73ced7315b71d9e6c60c166e19a9371
krishanathep <krishanathep@gmail.com>
2020-08-04 17:11:36 +0700

    Updates

---
+++ precache-manifest.aac6ff9f1a6e5b42e2e800bbdf24b430.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547929d8181bdfd637268f9a16111815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7203f4e5abde99a25f2d",
+    "url": "/static/css/2.95066baf.chunk.css"
+  },
+  {
+    "revision": "998b623ff9d60ba4b375",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7203f4e5abde99a25f2d",
+    "url": "/static/js/2.c504a9bf.chunk.js"
+  },
+  {
+    "revision": "bebd365a210ba9f0302c02dda8cd0832",
+    "url": "/static/js/2.c504a9bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998b623ff9d60ba4b375",
+    "url": "/static/js/main.610e8502.chunk.js"
+  },
+  {
+    "revision": "81fb37dc61825240b636",
+    "url": "/static/js/runtime-main.e4e7c846.js"
+  }
+]);
